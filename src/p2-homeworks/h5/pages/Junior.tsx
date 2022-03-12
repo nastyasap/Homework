94241de3627fae244e5e595aebd6d469c73311a2
Анастасия Сапожникова <nastyasap53@gmail.com>
2022-03-12 19:33:14 +0300

    DZ_4

---
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function Junior() {
+    return (
+        <div>
+            {/* <HW1/>
+            <HW2/>
+            <HW3/>
+            <HW4/>*/}
+
+        </div>
+    )
+}
+
+export default Junior
---
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function JuniorPlus() {
+    return (
+        <div>
+
+
+        </div>
+    )
+}
+
+export default JuniorPlus
+
